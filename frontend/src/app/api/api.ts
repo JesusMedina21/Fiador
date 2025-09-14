74a888d8aee5b1f8e9106f80ec52a5363f7a7092
JesusMedina21 <jesusmedina0921@gmail.com>
2025-09-14 13:22:23 -0400

    Update api.ts

--- frontend/src/app/api/api.ts
+++ frontend/src/app/api/api.ts
@@ -1,6 +1,5 @@
 export const api = {
-    apiUrl: "https://appfiador.vercel.app/api/" //Produccion
      
-    //apiUrl: "http://localhost:8000/api/" //Desarrollo
+    apiUrl: "http://localhost:8000/api/" //Desarrollo
     
 }
